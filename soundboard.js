7cfae7c9515d298e058f6172646a0663f7bd2f99
Nbatsch <batschyn@msu.edu>
2022-11-20 21:29:29 -0500

    completed + verified soundboard

---
+++ soundboard.js
@@ -0,0 +1,22 @@
+function snore() {
+    var audio = document.getElementById('snore');
+    audio.play();
+}
+
+function vineboom() {
+    var audio = document.getElementById('vineboom');
+    audio.play();
+
+}
+
+function legoyoda() {
+    var audio = document.getElementById('legoyoda');
+    audio.play();
+
+}
+
+
+/* Credit to Payspieloo for providing instruction on how to play a sound 
+on button click
+
+payspieloo.com/blog/how-to-play-a-sound-on-button-click-and-key-press-in-javascript*/
